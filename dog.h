08c2644dd2de6fbe155eabe9a7316003f879a06b
Heath-Thompson <36942178+Heath-Thompson@users.noreply.github.com>
2018-04-18 07:58:04 -0400

    Update dog.h

--- dog.h
+++ dog.h
@@ -1,4 +1,17 @@
-class Dog: public Card{
-	
-	
-}
+#ifndef DOG_H
+#define DOG_H
+
+#include <iostream>
+#include "card.h"
+
+using namespace std;
+
+class Dog : public Card {
+   // private:
+    
+    public:
+    Dog(string, int, int, int);
+    virtual string render(int);    
+};
+
+#endif
