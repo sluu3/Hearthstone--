2e85eb6ded84c0907a8ea7177a1c807afd8a950b
sluu3 <38304173+sluu3@users.noreply.github.com>
2018-04-23 08:12:46 -0400

    Giraffe Card Done

--- giraffe.cpp
+++ giraffe.cpp
@@ -5,12 +5,12 @@ string Giraffe::render(int line){
     switch(line){
         case 0: return ".___________.";
         case 1: return "|  Giraffe  |";
-        case 2: return "|           |";
-        case 3: return "|           |";
-        case 4: return "|           |";
-        case 5: return "|           |";
-        case 6: return "|           |";
-        case 7: return "|___________|";
+        case 2: return "|   ^_^     |";
+        case 3: return "|   \  \    |";
+        case 4: return "|    ----   |";
+        case 5: return "|     | |   |"
+        case 7: return "|   200/100 |";
+        case 6: return "|___________|";
         default:
             return " ";
     }
