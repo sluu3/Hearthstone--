43f2d7389b299652f6e1a1caaa7f1574cafbec2e
sluu3 <38304173+sluu3@users.noreply.github.com>
2018-04-23 08:17:24 -0400

    Hedgehog Card Done

--- hedgehog.cpp
+++ hedgehog.cpp
@@ -5,17 +5,18 @@ string Hedgehog::render(int line){
     switch(line){
         case 0: return ".___________.";
         case 1: return "| Hedgehog  |";
-        case 2: return "|           |";
-        case 3: return "|           |";
-        case 4: return "|           |";
+        case 2: return "|  \\\\\	|"
+		case 3: return "| \\\\\\\__.|"
+		case 4: return "|  \\\\\\\'/|";
         case 5: return "|           |";
-        case 6: return "|           |";
-        case 7: return "|___________|";
+        case 6: return "|   300/200 |";
+		case 7: return "|___________|";
+        
         default:
             return " ";
     }
 }
 
-Hedgehog::Hedgehog(void):Card("Hedgehog", 1, 200, 100){
+Hedgehog::Hedgehog(void):Card("Hedgehog", 1, 300, 200){
 	return;
 }
