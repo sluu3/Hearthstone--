37c705e8cbdfcf68e611fc52c26fba73a74b44af
Heath-Thompson <36942178+Heath-Thompson@users.noreply.github.com>
2018-04-24 20:50:12 -0400

    Update pig.cpp

--- pig.cpp
+++ pig.cpp
@@ -5,11 +5,11 @@ string Pig::render(int line){
     switch(line){
         case 0: return ".___________.";
         case 1: return "|    Pig    |";
-        case 2: return "|           |";
-        case 3: return "|           |";
-        case 4: return "|           |";
+        case 2: return "| '..'____  |";
+        case 3: return "| (@@)    |@|";
+        case 4: return "|     |- -| |";
         case 5: return "|           |";
-        case 6: return "|           |";
+        case 6: return "|  400/300  |";
         case 7: return "|___________|";
         default:
             return " ";
