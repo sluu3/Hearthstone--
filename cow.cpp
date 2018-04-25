564c475bb2f2d00c517ed4b124302ec30aeb6d98
Heath-Thompson <36942178+Heath-Thompson@users.noreply.github.com>
2018-04-24 20:39:26 -0400

    Update cow.cpp

--- cow.cpp
+++ cow.cpp
@@ -5,11 +5,11 @@ string Cow::render(int line){
     switch(line){
         case 0: return ".___________.";
         case 1: return "|    Cow    |";
-        case 2: return "|           |";
-        case 3: return "|           |";
-        case 4: return "|           |";
-        case 5: return "|           |";
-        case 6: return "|           |";
+        case 2: return "|   ()__()  |";
+        case 3: return "|   <@ @>   |";
+        case 4: return "|   {o_o}   |";
+        case 5: return "|    (|)    |";
+        case 6: return "|  300/400  |";
         case 7: return "|___________|";
         default:
             return " ";
