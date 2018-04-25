d2d30b678293b3955a612f14ea73fb88ce062d1a
Heath-Thompson <36942178+Heath-Thompson@users.noreply.github.com>
2018-04-24 20:42:13 -0400

    Update dog.cpp

--- dog.cpp
+++ dog.cpp
@@ -5,11 +5,11 @@ string Dog::render(int line){
     switch(line){
         case 0: return ".___________.";
         case 1: return "|    Dog    |";
-        case 2: return "|           |";
-        case 3: return "|           |";
-        case 4: return "|           |";
-        case 5: return "|           |";
-        case 6: return "|           |";
+        case 2: return "|   /^ ^\   |";
+        case 3: return "|  / 0 0 \  |";
+        case 4: return "|  V\ Y /V  |";
+        case 5: return "|   / - \   |";
+        case 6: return "|  400/400  |";
         case 7: return "|___________|";
         default:
             return " ";
