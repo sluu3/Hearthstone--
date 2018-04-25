7e14413d1582ef42bab9c7ed4be0486bc18537fe
Heath-Thompson <36942178+Heath-Thompson@users.noreply.github.com>
2018-04-24 20:45:07 -0400

    Update elephant.cpp

--- elephant.cpp
+++ elephant.cpp
@@ -5,11 +5,11 @@ string Elephant::render(int line){
     switch(line){
         case 0: return ".___________.";
         case 1: return "|  Elephant |";
-        case 2: return "|           |";
-        case 3: return "|           |";
-        case 4: return "|           |";
-        case 5: return "|           |";
-        case 6: return "|           |";
+        case 2: return "|  _ __ _   |";
+        case 3: return "| / |..| \  |";
+        case 4: return "| \/ || \/  |";
+        case 5: return "|  |_''_|   |";
+        case 6: return "|  800/800  |";
         case 7: return "|___________|";
         default:
             return " ";
