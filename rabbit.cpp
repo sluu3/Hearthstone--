53a38c112fd8ff0b38116f3700870187862607cd
Heath-Thompson <36942178+Heath-Thompson@users.noreply.github.com>
2018-04-24 20:52:14 -0400

    Update rabbit.cpp

--- rabbit.cpp
+++ rabbit.cpp
@@ -5,11 +5,11 @@ string Rabbit::render(int line){
     switch(line){
         case 0: return ".___________.";
         case 1: return "|   Rabbit  |";
-        case 2: return "|           |";
-        case 3: return "|           |";
-        case 4: return "|           |";
-        case 5: return "|           |";
-        case 6: return "|           |";
+        case 2: return "|  /)   (\  |";
+        case 3: return "|  \|   |/  |";
+        case 4: return "|   ( " )   |";
+        case 5: return "|     ^     |";
+        case 6: return "|  200/100  |";
         case 7: return "|___________|";
         default:
             return " ";
