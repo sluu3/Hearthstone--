30f98dae1ef9317c5eba3ea355c6459af02fc98b
Heath-Thompson <36942178+Heath-Thompson@users.noreply.github.com>
2018-04-24 20:46:56 -0400

    Update frog.cpp

--- frog.cpp
+++ frog.cpp
@@ -5,11 +5,11 @@ string Frog::render(int line){
     switch(line){
         case 0: return ".___________.";
         case 1: return "|    Frog   |";
-        case 2: return "|           |";
-        case 3: return "|           |";
-        case 4: return "|           |";
+        case 2: return "|  (1)-(1)  |";
+        case 3: return "|  /_____\  |";
+        case 4: return "|  \_____/  |";
         case 5: return "|           |";
-        case 6: return "|           |";
+        case 6: return "|  200/100  |";
         case 7: return "|___________|";
         default:
             return " ";
