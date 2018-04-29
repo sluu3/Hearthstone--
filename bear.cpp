35864d523c325babd51fd1e7255adae0d17447cc
Heath-Thompson <36942178+Heath-Thompson@users.noreply.github.com>
2018-04-29 14:49:54 -0400

    Update bear.cpp

--- bear.cpp
+++ bear.cpp
@@ -6,7 +6,7 @@ string Bear::render(int line){
         case 0: return ".___________.";
         case 1: return "|   Bear    |";
         case 2: return "|           |";
-        case 3: return "|  /^\\ /^\\  |";
+        case 3: return "|  /^\\ /^\\|";
         case 4: return "| ('o(I)o') |";
         case 5: return "|           |";
         case 6: return "|  200/100  |";
